10467ac0bfc2687513d03fc5926a22ab8a204d1c
Etse-Sol <solomonetsegenet7@gmail.com>
2025-01-25 08:13:17 +0300

    final

--- client/src/Components/config/axios.js
+++ client/src/Components/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-	baseURL: "http://localhost:4000/api",
+  baseURL: "https://evangadi-forum-backend-1-rvth.onrender.com/Api",
 });
 
 export default axiosBase;
